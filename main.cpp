7d52c22b9b8b8143d5a165b626a83c5a9a0fa60e
Aleksey Smirnov <55441212+leshastern@users.noreply.github.com>
2020-06-04 18:50:46 +0300

    create main.cpp

---
+++ main.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Shop
+{
+public:
+	virtual string payment() const = 0;
+};
+
+class Context
+{
+private:
+	Shop* strategy_;
+	
+public:
+	Context(Shop* strategy = nullptr) : strategy_(strategy)
+	{
+	}
+	~Context()
+	{
+		delete this->strategy_;
+	}
+
+	void set_strategy(Shop* strategy)
+	{
+		delete this->strategy_;
+		this->strategy_ = strategy;
+	}
+	
+	void DoSomeBusinessLogic() const
+	{
+		
+		string result = this->strategy_->DoAlgorithm();
+		cout << result << endl;
+	}
+};
+
+
+class Cash : public Shop
+{
+public:
+	string payment() const override
+	{
+		return "Cash, skidka";
+	}
+};
+
+class Card : public Shop
+{
+public:
+	string payment() const override
+	{
+		return "Card, bonuses";
+	}
+};
+
+
+int main()
+{
+	int N;
+	cout << "Enter sum (km/h)\n";
+	cin >> N;
+
+	Context* context = new Context();
+
+	if (N <= 0) cout << "Error: invalid data" << endl;
+	else if (N < 5000) context->set_strategy(new Cash);
+	else if context->set_strategy(new Card);
+
+	context->DoSomeBusinessLogic();
+	delete context;
+
+	system("PAUSE");
+	return 0;
+}
